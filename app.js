a190c6051f4a10b4193324a19e29e1f981f057e1
Hack Reactor Students <accounts@hackreactor.com>
2017-01-06 17:00:19 -0800

    Completed ReactComponents

--- app.js
+++ app.js
@@ -1,1 +1,53 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hovered: false
+    };
+  }
+
+  onGroceryListItemHover() {
+    this.setState({
+      hovered: !this.state.hovered
+    });
+  }
+
+  render() {
+    var style = {
+      'font-weight': this.state.hovered ? 'bold' : 'normal'
+    };
+
+
+
+    return (
+      // <li>{this.props.item}</li>
+      <li style={style} onMouseEnter={this.onGroceryListItemHover.bind(this)} 
+                        onMouseLeave={this.onGroceryListItemHover.bind(this)}>
+        {this.props.item}
+      </li>
+    );
+  }
+}
+
+
+var GroceryList = (props) => (
+  // {
+  // var rows = [];
+  // var groceryItems = props.groceryItems;
+  // for (var i = 0; i < groceryItems.length; i++){
+  //   rows.push(<GroceryListItem item = {groceryItems[i]}/>);
+  // }
+  
+  // return (<ul>{rows}</ul>);
+  <ul>
+    {props.groceryItems.map(item => 
+      <GroceryListItem item={item} />
+    )}
+  </ul>
+);
+
+// var GroceryListItem = (props) => (
+// );
+
+ReactDOM.render(<GroceryList groceryItems = {['blah', 'bleh', 'vegetables']}/>, document.getElementById("app"));
