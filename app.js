9ad25114b1abe2d86e8cc213ccf5d1470bc8d040
Joey Lee <jleepianist@gmail.com>
2017-05-05 23:12:00 -0700

    (refactor/code cleanup) Removed comments
    Also, refactored indentations to better reflect recommended React style guide idiom

--- app.js
+++ app.js
@@ -3,51 +3,45 @@ class GroceryListItem extends React.Component {
     super(props);
 
     this.state = {
-      hovered: false
-    };
+      hovering: false
+    }
   }
 
-  onGroceryListItemHover() {
+  onListItemMouseEnter() {
     this.setState({
-      hovered: !this.state.hovered
+      hovering: true
     });
+  }
+
+  onListItemMouseLeave() {
+    this.setState({
+      hovering: false
+    })
   }
 
   render() {
     var style = {
-      'font-weight': this.state.hovered ? 'bold' : 'normal'
+      fontWeight: this.state.hovering ? 'bold' : 'normal'
     };
 
-
-
     return (
-      // <li>{this.props.item}</li>
-      <li style={style} onMouseEnter={this.onGroceryListItemHover.bind(this)} 
-                        onMouseLeave={this.onGroceryListItemHover.bind(this)}>
+      <li
+        style={style}
+        onMouseLeave={this.onListItemMouseLeave.bind(this)}
+        onMouseEnter={this.onListItemMouseEnter.bind(this)}
+      >
         {this.props.item}
       </li>
-    );
+    )
   }
 }
 
-
 var GroceryList = (props) => (
-  // {
-  // var rows = [];
-  // var groceryItems = props.groceryItems;
-  // for (var i = 0; i < groceryItems.length; i++){
-  //   rows.push(<GroceryListItem item = {groceryItems[i]}/>);
-  // }
-  
-  // return (<ul>{rows}</ul>);
   <ul>
-    {props.groceryItems.map(item => 
+    {props.items.map(item =>
       <GroceryListItem item={item} />
     )}
   </ul>
 );
 
-// var GroceryListItem = (props) => (
-// );
-
-ReactDOM.render(<GroceryList groceryItems = {['blah', 'bleh', 'vegetables']}/>, document.getElementById("app"));
+ReactDOM.render(<GroceryList items={['kale', 'cucumbers']} />, document.getElementById('app'));
